49d46069af3bcb4d0ae2cbb0b062b04c6573305e
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-06-16 13:51:34 +0100

    fix: load exchange rates token from env

--- supabase/functions/proxy-live-rates/index.ts
+++ supabase/functions/proxy-live-rates/index.ts
@@ -3,6 +3,5 @@ import { serve } from 'https://deno.land/std@0.192.0/http/server.ts'
 import { handleRequest } from './handler.ts'
 
 serve((req: Request) => {
-  const token = Deno.env.get('OPENEXCHANGERATES_TOKEN') || ''
-  return handleRequest(req, token, fetch)
+  return handleRequest(req, fetch)
 })
