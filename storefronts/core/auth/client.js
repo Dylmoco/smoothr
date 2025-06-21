bba07ed745a9d1a9304f4cc5188aa75441764995
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-06-21 07:31:54 +0100

    refactor: share supabase client

---
+++ storefronts/core/auth/client.js
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const DEFAULT_SUPABASE_URL =
+  (typeof __NEXT_PUBLIC_SUPABASE_URL__ !== 'undefined' && __NEXT_PUBLIC_SUPABASE_URL__) ||
+  'https://your-project.supabase.co';
+export const DEFAULT_SUPABASE_KEY =
+  (typeof __NEXT_PUBLIC_SUPABASE_ANON_KEY__ !== 'undefined' && __NEXT_PUBLIC_SUPABASE_ANON_KEY__) ||
+  'your-anon-key';
+
+const supabase = createClient(DEFAULT_SUPABASE_URL, DEFAULT_SUPABASE_KEY);
+
+export default supabase;
