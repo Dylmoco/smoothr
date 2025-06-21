52c81008b97e9d212cc7b44ec5babeba43036071
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-06-21 05:23:03 +0100

    fix vite config dirname

--- storefronts/vite.config.js
+++ storefronts/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
