55fe9f1c78bded56f18bdc1db176a68d623d8d9f
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-07-09 22:43:46 +0100

    Add log for provider checkout API

--- smoothr/pages/api/checkout/[provider].ts
+++ smoothr/pages/api/checkout/[provider].ts
@@ -3,6 +3,7 @@ import '../../../../shared/init';
 import { handleCheckout } from 'shared/checkout/handleCheckout';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('[API] 🔥 [provider] API route hit');
   if (req.method !== 'POST' && req.method !== 'OPTIONS') {
     res.setHeader('Allow', 'POST, OPTIONS');
     res.status(405).end();
