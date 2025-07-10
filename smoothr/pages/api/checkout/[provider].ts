0c874f7fe129bd31a937c4e2e3b91e4fb108a44c
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-07-10 23:16:28 +0100

    feat(api): add CORS support

--- smoothr/pages/api/checkout/[provider].ts
+++ smoothr/pages/api/checkout/[provider].ts
@@ -3,6 +3,17 @@ import '../../../../shared/init';
 import { handleCheckout } from 'shared/checkout/handleCheckout';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'OPTIONS') {
+    return res
+      .status(200)
+      .setHeader('Access-Control-Allow-Origin', 'https://smoothr-cms.webflow.io')
+      .setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+      .setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      .end();
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://smoothr-cms.webflow.io');
+
   console.log('[API] 🔥 [provider] API route hit');
   if (req.method !== 'POST' && req.method !== 'OPTIONS') {
     res.setHeader('Allow', 'POST, OPTIONS');
--- smoothr/pages/api/create-order.ts
+++ smoothr/pages/api/create-order.ts
@@ -9,6 +9,16 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  if (req.method === 'OPTIONS') {
+    return res
+      .status(200)
+      .setHeader('Access-Control-Allow-Origin', 'https://smoothr-cms.webflow.io')
+      .setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+      .setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      .end();
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://smoothr-cms.webflow.io');
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
