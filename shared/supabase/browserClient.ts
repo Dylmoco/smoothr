5e7d4a4fcefd4596c074b8000aa18e1f3f991569
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-07-24 17:46:45 +0100

    fix: configure supabase client for tests

--- shared/supabase/browserClient.ts
+++ shared/supabase/browserClient.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const options = process.env.NODE_ENV === 'test'
+  ? { auth: { autoRefreshToken: false, persistSession: false } }
+  : {};
+
+const supabase = createClient(url, key, options);
 
 export { supabase };
