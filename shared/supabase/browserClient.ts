14398cc4098db17d428c67bbdb7064f64d14b330
Dylan <dmocojunk@gmail.com>
2025-08-04 10:20:36 +0100

    fix

--- shared/supabase/browserClient.ts
+++ shared/supabase/browserClient.ts
@@ -2,18 +2,24 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 async function applySessionAuth() {
   try {
     if (typeof process !== 'undefined' && process.env.NODE_ENV === 'test') return;
+
     const {
       data: { session }
     } = await supabase.auth.getSession();
+
     const token = session?.access_token;
+
     if (token) {
-      supabase.auth.setAuth(token);
+      await supabase.auth.setSession({
+        access_token: token,
+        refresh_token: '' // no need for refresh token in this use case
+      });
     }
   } catch {
     // ignore errors
