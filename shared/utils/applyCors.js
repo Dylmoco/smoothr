b76abbe92d076de8a6b4f1f4c7fa162c7cf5e0e9
Dylan <dmocojunk@gmail.com>
2025-08-10 17:59:32 +0100

    fix tests

---
+++ shared/utils/applyCors.js
@@ -0,0 +1,6 @@
+export function applyCors(res, origin = '*') {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  }
+  
