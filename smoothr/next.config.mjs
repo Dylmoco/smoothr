ebfbaa84a4b12ee2da891b91750815b0a0cae1bb
Dylan <73825106+Dylmoco@users.noreply.github.com>
2025-09-05 04:46:07 +0100

    Auth: Update callback page

--- smoothr/next.config.mjs
+++ smoothr/next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
   experimental: {
     externalDir: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/auth/callback',
+        headers: [
+          { key: 'Cross-Origin-Opener-Policy', value: 'unsafe-none' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'unsafe-none' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
